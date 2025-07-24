60f054dc102fb8b4f5d83fd672bd400690227c62
VistLiPwr <732033049@qq.com>
2025-07-24 21:08:18 +0800

    08 09 10

--- exercises/08_pointer/main.cpp
+++ exercises/08_pointer/main.cpp
@@ -5,6 +5,17 @@ bool is_fibonacci(int *ptr, int len, int stride) {
     ASSERT(len >= 3, "`len` should be at least 3");
     // TODO: 编写代码判断从 ptr 开始，每 stride 个元素取 1 个元素，组成长度为 n 的数列是否满足
     // arr[i + 2] = arr[i] + arr[i + 1]
+     for (int i = 0; i < len - 2; i++) {
+        // 计算当前元素、下一个元素和下下个元素的地址
+        int *current = ptr + i * stride;
+        int *next = ptr + (i + 1) * stride;
+        int *next_next = ptr + (i + 2) * stride;
+        
+        // 检查是否满足斐波那契条件
+        if (*next_next != *current + *next) {
+            return false;
+        }
+    }
     return true;
 }
 
